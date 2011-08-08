3120c721e879bb89c3bb77e23deac75b93f4499c
Marc Hoeppner <mh@imbim090.imbim.uu.se>
2011-08-08 13:08:33 +0200

    restructuring of libs

---
+++ lib/files/db_connection.rb
@@ -0,0 +1,32 @@
+# DB Connection class
+
+Rfam_DB_ADAPTER = 'mysql'
+Rfam_DATABASE = "rfam_"
+Rfam_DB_HOST = 'localhost'
+Rfam_DB_USERNAME = ''
+Rfam_DB_PASSWORD = ''
+
+module Toolbox 
+  
+  module RfamDB
+    
+    class DBConnection < ActiveRecord::Base
+      self.abstract_class = true
+    
+      def self.connect(version="_91")
+
+        establish_connection(
+                              :adapter => Rfam_DB_ADAPTER,
+                              :host => Rfam_DB_HOST,
+                              :database => "#{Rfam_DATABASE}#{version}",
+                              :username => Rfam_DB_USERNAME,
+                              :password => Rfam_DB_PASSWORD
+                              #:port => port
+                            )
+      end
+    
+    end
+    
+  end
+  
+end
